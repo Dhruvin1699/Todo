56a01dd4dd1f4af7e00e400db55f0517acca1655
Dhruvin1699 <dhruvil.ptridhyatech@gmail.com>
2023-10-11 11:27:15 +0530

    changes App Icon and Name

--- lib/Presentation/Screens/homescreen.dart
+++ lib/Presentation/Screens/homescreen.dart
@@ -1,314 +1,9 @@
-// import 'package:flutter/material.dart';
-// import 'package:todoappdemo/Presentation/Screens/edit.dart';
-// import 'package:todoappdemo/Presentation/Screens/tas.dart';
-// import 'package:todoappdemo/databse.dart';
-//
-// void main() => runApp(MyApp());
-//
-// class MyApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: TodoAppHomeScreen(),
-//     );
-//   }
-// }
-//
-// class TodoAppHomeScreen extends StatefulWidget {
-//   @override
-//   _TodoAppHomeScreenState createState() => _TodoAppHomeScreenState();
-// }
-//
-// class _TodoAppHomeScreenState extends State<TodoAppHomeScreen> {
-//   String? _selectedOption; // Variable to store the selected dropdown option
-//   TextEditingController _textFieldController =
-//   TextEditingController(); // Controller for the text field
-//   List<TaskModel> _tempTasks = [];
-//   DatabaseHelper _dbHelper = DatabaseHelper();
-//   List<TaskModel> _tasks = [];
-//
-//
-//   void _navigateToTaskScreen() async {
-//     String? selectedItem = await Navigator.push(
-//       context,
-//       MaterialPageRoute(builder: (context) => Task()),
-//     );
-//
-//     if (selectedItem != null) {
-//       _loadTasks(); // Refresh tasks when returning from the Task screen
-//     }
-//   }
-//   @override
-//   void initState() {
-//     super.initState();
-//     _loadTasks();
-//   }
-//
-//   Future<void> _loadTasks() async {
-//     print('Loading tasks...'); // Add this line for debugging
-//     List<TaskModel> tasks = await _dbHelper.getTasks();
-//     setState(() {
-//       _tasks = tasks;
-//     });
-//   }
-//   Future<bool> _showDeleteConfirmationDialog(BuildContext context) async {
-//     bool confirmDelete = false;
-//     await showDialog(
-//       context: context,
-//       builder: (BuildContext context) {
-//         return AlertDialog(
-//           title: Text("Confirm Deletion"),
-//           content: Text("Are you sure you want to delete this task?"),
-//           actions: <Widget>[
-//             TextButton(
-//               child: Text("Cancel"),
-//               onPressed: () {
-//                 Navigator.of(context).pop(false); // Return false to indicate cancellation
-//               },
-//             ),
-//             TextButton(
-//               child: Text("Yes"),
-//               onPressed: () {
-//                 confirmDelete = true;
-//                 Navigator.of(context).pop(true); // Return true to confirm deletion
-//               },
-//             ),
-//           ],
-//         );
-//       },
-//     );
-//     return confirmDelete;
-//   }
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       floatingActionButton: Padding(
-//         padding: const EdgeInsets.only(bottom: 80.0),
-//         child: FloatingActionButton(
-//           onPressed: () {
-//             _navigateToTaskScreen();
-//           },
-//           child: Icon(Icons.add, color: Colors.black),
-//           backgroundColor: Colors.grey,
-//         ),
-//       ),
-//       appBar: AppBar(
-//         backgroundColor: Colors.white,
-//         titleSpacing: 0,
-//         leading: Padding(
-//           padding: const EdgeInsets.all(12.0),
-//           child: Container(
-//             width: 20, // Set the width of the container
-//             height: 10, // Set the height of the container
-//             child: Image.asset(
-//               'images/pngwing.com (1).png', //
-//             ),
-//           ),
-//         ),
-//         title: Row(
-//           mainAxisAlignment: MainAxisAlignment.start,
-//           children: [
-//             Padding(
-//               padding: const EdgeInsets.only(left: 1.0),
-//               child: Text(
-//                 'Custom Title',
-//                 style: TextStyle(color: Colors.black),
-//               ),
-//             ), // Your custom title
-//             // Add some space between title and dropdown icon
-//             DropdownButtonHideUnderline(
-//               // Hide the underline
-//               child: DropdownButton<String>(
-//                 // Dropdown menu
-//                 value: _selectedOption, // Selected option value
-//                 icon: Icon(Icons.arrow_drop_down), // Custom dropdown icon
-//                 items: <String>['Option 1', 'Option 2', 'Option 3']
-//                     .map((String value) {
-//                   return DropdownMenuItem<String>(
-//                     value: value,
-//                     child: Text(value),
-//                   );
-//                 }).toList(),
-//                 onChanged: (String? newValue) {
-//                   setState(() {
-//                     _selectedOption = newValue; // Update the selected option
-//                   });
-//                   // Handle dropdown menu selection
-//                 },
-//               ),
-//             ),
-//           ],
-//         ),
-//         actions: <Widget>[
-//           IconButton(
-//             icon: Icon(Icons.search, color: Colors.black), // Search icon
-//             onPressed: () {
-//               // Implement your search functionality here
-//             },
-//           ),
-//           PopupMenuButton<String>(
-//             color: Colors.black,
-//             onSelected: (String choice) {
-//               // Handle choice selection
-//               // Implement your action icon functionality here
-//             },
-//             itemBuilder: (BuildContext context) {
-//               return <PopupMenuEntry<String>>[
-//                 PopupMenuItem<String>(
-//                   value: 'Option 1',
-//                   child: Text('Option 1'),
-//                 ),
-//                 PopupMenuItem<String>(
-//                   value: 'Option 2',
-//                   child: Text('Option 2'),
-//                 ),
-//                 PopupMenuItem<String>(
-//                   value: 'Option 3',
-//                   child: Text('Option 3'),
-//                 ),
-//               ];
-//             },
-//           ),
-//         ],
-//       ),
-//       body: Column(
-//         children: <Widget>[
-//           // Other body content goes here
-//
-//           // Text field at the bottom
-//           Expanded(child:
-//
-//
-//           ListView.builder(
-//             itemCount: _tasks.length,
-//             itemBuilder: (context, index) {
-//               TaskModel task = _tasks[index];
-//
-//               return Dismissible(
-//                 key: UniqueKey(),
-//                 onDismissed: (direction) async {
-//                   // Store tasks in _tempTasks
-//                   _tempTasks = List.from(_tasks);
-//                   // Show a confirmation dialog
-//                   bool confirmDelete = await _showDeleteConfirmationDialog(context);
-//                   if (confirmDelete) {
-//                     // Delete the task from the database
-//                     await _dbHelper.deleteTask(task.id);
-//                     // Update the UI to remove the dismissed item
-//                     setState(() {
-//                       _tasks.removeAt(index);
-//                     });
-//                   } else {
-//                     // Restore tasks from _tempTasks on cancel
-//                     setState(() {
-//                       _tasks = List.from(_tempTasks);
-//                     });
-//                   }
-//                 },
-//                 background: Container(
-//                   color: Colors.red,
-//                   alignment: Alignment.centerRight,
-//                   padding: EdgeInsets.only(right: 16.0),
-//                   child: Icon(Icons.delete, color: Colors.white),
-//                 ),
-//                 secondaryBackground: Container(
-//                   color: Colors.green,
-//                   alignment: Alignment.centerLeft,
-//                   padding: EdgeInsets.only(left: 16.0),
-//                   child: Icon(Icons.check, color: Colors.white),
-//                 ),
-//                 child: GestureDetector(
-//                   onTap: () async {
-//                     // Navigate to the task page with the selected task
-//                     TaskModel updatedTask = await Navigator.push(
-//                       context,
-//                       MaterialPageRoute(
-//                         builder: (context) => TaskPage(task: task),
-//                       ),
-//                     );
-//
-//                     // If the task was updated, update the task in the local list
-//                     if (updatedTask != null) {
-//                       setState(() {
-//                         int taskIndex = _tasks.indexWhere((element) => element.id == updatedTask.id);
-//                         if (taskIndex != -1) {
-//                           _tasks[taskIndex] = updatedTask;
-//                         }
-//                       });
-//                     }
-//                   },
-//                   child: Padding(
-//                     padding: const EdgeInsets.all(8.0),
-//                     child: Container(
-//                       padding: const EdgeInsets.all(16.0),
-//                       decoration: BoxDecoration(
-//                         border: Border.all(color: Colors.grey),
-//                         borderRadius: BorderRadius.circular(8.0),
-//                       ),
-//                       child: Column(
-//                         crossAxisAlignment: CrossAxisAlignment.start,
-//                         children: [
-//                           Row(
-//                             mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//                             children: [
-//                               Text(
-//                                 task.task,
-//                                 style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.black),
-//                               ),
-//                               Text(
-//                                 task.date,
-//                                 style: TextStyle(fontSize: 14, color: Colors.grey),
-//                               ),
-//                             ],
-//                           ),
-//                           SizedBox(height: 10),
-//                           Text(
-//                             task.selectedItem,
-//                             style: TextStyle(fontSize: 16, color: Colors.blue),
-//                           ),
-//                         ],
-//                       ),
-//                     ),
-//                   ),
-//                 ),
-//               );
-//             },
-//           )
-//           ),
-//           Container(
-//             height: 60,
-//             decoration: BoxDecoration(
-//               border: Border.all(color: Colors.grey), // Border line color
-//               borderRadius: BorderRadius.circular(8),
-//             ),
-//             margin: EdgeInsets.all(16),
-//             child: Padding(
-//               padding: const EdgeInsets.all(8.0),
-//               child: TextField(
-//                 controller: _textFieldController,
-//
-//                 decoration: InputDecoration(
-//                   hintText: 'Enter your text', // Hint text
-//                   focusedBorder: UnderlineInputBorder(
-//                     borderSide: BorderSide(color: Colors
-//                         .green), // Underline decoration without boxes
-//                   ),
-//                 ),
-//               ),
-//             ),
-//           )
-//         ],
-//       ),
-//     );
-//   }
-//
-// }
-//
-//
+
 
 import 'package:flutter/material.dart';
 import 'package:todoappdemo/Presentation/Screens/edit.dart';
 import 'package:todoappdemo/Presentation/Screens/tas.dart';
+import 'package:todoappdemo/Presentation/Screens/tasklist.dart';
 import 'package:todoappdemo/databse.dart';
 
 void main() => runApp(MyApp());
@@ -413,7 +108,7 @@ class _TodoAppHomeScreenState extends State<TodoAppHomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: Padding(
-        padding: const EdgeInsets.only(bottom: 80.0),
+        padding: const EdgeInsets.only(bottom: 0.0),
         child: FloatingActionButton(
           onPressed: () {
             TaskModel newTask = TaskModel(
@@ -427,6 +122,7 @@ class _TodoAppHomeScreenState extends State<TodoAppHomeScreen> {
           child: Icon(Icons.add,     color:Color(0xFF01579B),),
           backgroundColor: Colors.white,
         ),
+
       ),
      appBar: AppBar(
          backgroundColor: Color(0xFF0277BD),
@@ -486,49 +182,83 @@ class _TodoAppHomeScreenState extends State<TodoAppHomeScreen> {
               // Implement your search functionality here
             },
           ),
+
           PopupMenuButton<String>(
             color: Colors.white, // Set background color of the popup menu
-            onSelected: (String choice) {
-              // Handle choice selection
-            },
             icon: Icon(
               Icons.more_vert,
               color: Colors.white, // Set icon color to black
             ),
             itemBuilder: (BuildContext context) {
-              return <PopupMenuEntry<String>>[
-                PopupMenuItem<String>(
-                  value: 'Option 1',
+              return [
+                'TaskList',
+                'Send Feedback',
+                'Invite friends to the app',
+                'Setting',
+              ].map((String option) {
+                return PopupMenuItem<String>(
+                  value: option,
                   child: Text(
-                    'TaskList',
+                    option,
                     style: TextStyle(color: Colors.black), // Set text color to black
                   ),
-                ),
-                PopupMenuItem<String>(
-                  value: 'Option 2',
-                  child: Text(
-                    'Send Feedback',
-                    style: TextStyle(color: Colors.black), // Set text color to black
-                  ),
-                ),
-                PopupMenuItem<String>(
-                  value: 'Option 3',
-                  child: Text(
-                    'Invite friends to the app',
-                    style: TextStyle(color: Colors.black), // Set text color to black
-                  ),
-                ),
-                PopupMenuItem<String>(
-                  value: 'Option 4',
-                  child: Text(
-                    'Setting',
-                    style: TextStyle(color: Colors.black), // Set text color to black
-                  ),
-                ),
-              ];
+                );
+              }).toList();
             },
-          ),
+            onSelected: (String choice) {
+              // Handle choice selection
+              if (choice == 'TaskList') {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => TaskListScreen(tasks:_tasks),
+                  ),
+                );
+              } else if (choice == 'Send Feedback') {
+                // Handle 'Send Feedback' option
+              } else if (choice == 'Invite friends to the app') {
+                // Handle 'Invite friends to the app' option
+              } else if (choice == 'Setting') {
+                // Handle 'Setting' option
+              }
+            },
+          )
         ],
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: Color(0xFF0277BD),
+        child: Container(
+          height: 60,
+          margin: EdgeInsets.symmetric(horizontal: 0),
+          child: Row(
+            children: [
+              IconButton(
+                onPressed: () {
+                  // Handle speech recognition here
+                },
+                icon: Icon(Icons.mic, color: Colors.white),
+              ),
+              Expanded(
+                child: TextField(
+                  controller: _textFieldController,
+                  style: TextStyle(color: Colors.white),
+                  decoration: InputDecoration(
+                    hintText: 'Enter your text',
+                    hintStyle: TextStyle(fontSize: 14, color: Colors.white70),
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white60),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.transparent),
+                    ),
+                  ),
+                ),
+              ),
+
+
+            ],
+          ),
+        ),
       ),
       body: Container(
          color:Color(0xFF01579B),
@@ -547,6 +277,7 @@ class _TodoAppHomeScreenState extends State<TodoAppHomeScreen> {
             TaskModel task = _tasks[index];
             if (_selectedOption == 'All' || task.selectedItem == _selectedOption) {
               return Dismissible(
+                  direction: DismissDirection.startToEnd,
                 key: UniqueKey(),
                 onDismissed: (direction) async {
                   bool confirmDelete = await _showDeleteConfirmationDialog(context);
@@ -555,7 +286,7 @@ class _TodoAppHomeScreenState extends State<TodoAppHomeScreen> {
                     _loadTasks();
                   }else{
                     setState(() {
-                      _tasks = List.from(_tempTasks);
+                      _tasks = List.from(_tasks );
                     });
                   }
                 },
@@ -675,28 +406,31 @@ class _TodoAppHomeScreenState extends State<TodoAppHomeScreen> {
     ),
 
 
-            Container(
-              height: 60,
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.grey), // Border line color
-                borderRadius: BorderRadius.circular(8),
-              ),
-              margin: EdgeInsets.all(16),
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextField(
-                  controller: _textFieldController,
-
-                  decoration: InputDecoration(
-                    hintText: 'Enter your text', // Hint text
-                    focusedBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(color: Colors
-                          .green), // Underline decoration without boxes
-                    ),
-                  ),
-                ),
-              ),
-            )
+            // Container(
+            //   height: 60,
+            //   decoration: BoxDecoration(
+            //     border: Border.all(color: Colors.grey), // Border line color
+            //     borderRadius: BorderRadius.circular(8),
+            //   ),
+            //   margin: EdgeInsets.all(16),
+            //   child: Padding(
+            //     padding: const EdgeInsets.all(8.0),
+            //     child: TextField(
+            //       controller: _textFieldController,
+            //
+            //       decoration: InputDecoration(
+            //         hintText: 'Enter your text',hintStyle: TextStyle(fontSize: 10,color: Colors.white), // Hint text
+            //         enabledBorder: UnderlineInputBorder(
+            //           borderSide: BorderSide(color: Colors.white), // Manually set the underline color to white
+            //         ),
+            //         focusedBorder: UnderlineInputBorder(
+            //           borderSide: BorderSide(color: Colors
+            //               .white), // Underline decoration without boxes
+            //         ),
+            //       ),
+            //     ),
+            //   ),
+            // )
           ],
         ),
       ),
---
+++ lib/Presentation/Screens/tasklist.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:todoappdemo/Presentation/Screens/tas.dart';
+
+
+import '../../databse.dart'; // Import your TaskModel class
+class TaskListScreen extends StatefulWidget {
+  final List<TaskModel> tasks;
+
+  TaskListScreen({required this.tasks});
+
+  @override
+  State<TaskListScreen> createState() => _TaskListScreenState();
+}
+
+class _TaskListScreenState extends State<TaskListScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xFF0277BD),
+        title: Text('Task List'),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.of(context).pop(); // Navigate back when the back arrow is pressed
+          },
+        ),
+      ),
+      body: Container(
+        color:Color(0xFF01579B),
+        child: ListView.builder(
+          itemCount: widget.tasks.length,
+          itemBuilder: (context, index) {
+            TaskModel task = widget.tasks[index];
+            return ListTile(
+              title: Text(task.task,style: TextStyle(color: Colors.white),),
+              subtitle: Text(task.date,style: TextStyle(color: Colors.white60),),
+              trailing: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  IconButton(
+                    icon: Icon(Icons.edit,color: Colors.white60,),
+                    onPressed: () {
+                      // Navigate to Task screen in edit mode
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => Task(onItemAdded: (String newItem) {}, task: task, isEdit: true),
+                        ),
+                      );
+                    },
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.delete,color: Colors.white60),
+                    onPressed: () {
+                      // Handle delete functionality here
+                    },
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
